f30f11d07275459e8d5faf5106a34ea48b440d83
OM Sharma <osharma311@hotmail.com>
2022-04-10 02:52:27 +0530

    added polymorphism

--- CalculatorDemo/Program.cs
+++ CalculatorDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CalculatorDemo
 {
@@ -6,7 +7,7 @@ namespace CalculatorDemo
     {
         public static void Main(string[] args)
         {
-            for (; ; ) 
+            for (; ; )
             {
                 int firstNum = 0;
                 int secondNum = 0;
@@ -18,13 +19,13 @@ namespace CalculatorDemo
                 do
                 {
                     key = Console.ReadKey(true);
-                        int val = 0;
-                        bool _x = Int32.TryParse(key.KeyChar.ToString(), out val);
-                        if (_x)
-                        {
-                            _val += key.KeyChar;
-                            Console.Write(key.KeyChar);
-                        }
+                    int val = 0;
+                    bool _x = Int32.TryParse(key.KeyChar.ToString(), out val);
+                    if (_x)
+                    {
+                        _val += key.KeyChar;
+                        Console.Write(key.KeyChar);
+                    }
                 }
                 while (key.Key != ConsoleKey.Enter);
                 firstNum = Convert.ToInt32(_val);
@@ -34,12 +35,12 @@ namespace CalculatorDemo
                 {
                     key = Console.ReadKey(true);
                     int val2 = 0;
-                        bool _x = Int32.TryParse(key.KeyChar.ToString(), out val2);
-                        if (_x)
-                        {
-                            _val2 += key.KeyChar;
-                            Console.Write(key.KeyChar);
-                        }
+                    bool _x = Int32.TryParse(key.KeyChar.ToString(), out val2);
+                    if (_x)
+                    {
+                        _val2 += key.KeyChar;
+                        Console.Write(key.KeyChar);
+                    }
                 }
                 while (key.Key != ConsoleKey.Enter);
 
@@ -49,16 +50,90 @@ namespace CalculatorDemo
                 char operation = Convert.ToChar(Console.ReadLine());
 
                 int result = 0;
-                switch (operation)
+                if(operation=='+')
                 {
-                    case '+': result = firstNum + secondNum; break;
-                    case '-': result = firstNum - secondNum; break;
-                    case '*': result = firstNum * secondNum; break;
-                    case '/': result = firstNum / secondNum; break;
-                    case '%': result = firstNum % secondNum; break;
+                    Add add = new Add();
+                    result= add.Calculate(firstNum,secondNum);
                 }
+                else if(operation=='-')
+                {
+                    Substract substract = new Substract();
+                    result= substract.Calculate(firstNum,secondNum);
+                }
+                else if(operation=='*')
+                { 
+                    Multiply multiply= new Multiply();
+                    result= multiply.Calculate(firstNum,secondNum);
+                }
+                else if(operation=='/')
+                { 
+                    Divide divide= new Divide();
+                    result= divide.Calculate(firstNum,secondNum);
+                }
+                else if(operation=='%')
+                { 
+                    Remainder remainder= new Remainder();
+                    result= remainder.Calculate(firstNum,secondNum);
+                }
+                else
+                {
+                    CalculateValue calculate = new CalculateValue();
+                    result= calculate.Calculate(firstNum,secondNum);
+                }
+                //string filePath= @"E:\C# Practise\CalculatorDemo\Content\log.txt";
+                //FileInfo fileInfo = new FileInfo(filePath);
+
+                //fileInfo.AppendText(result);
                 Console.WriteLine("{0} and {1} with Operator {2} result in {3}", firstNum, secondNum, operation, result);
             }
+        }
+    }
+    public class CalculateValue
+    {
+        public virtual int Calculate(int firstNum, int secondNum)
+        {
+             int output = 0;
+            return output;
+        }
+    }
+    public class Add : CalculateValue
+    {
+        public override int Calculate(int firstNum, int secondNum)
+        {
+              int output = firstNum + secondNum;
+            return output;
+        }
+    }
+    public class Substract : CalculateValue
+    {
+        public override int Calculate(int firstNum, int secondNum)
+        {
+             int output = firstNum - secondNum;
+            return output;
+        }
+    }
+    public class Multiply : CalculateValue
+    {
+        public override int Calculate(int firstNum, int secondNum)
+        {
+             int output = firstNum * secondNum;
+            return output;
+        }
+    }
+    public class Divide : CalculateValue
+    {
+        public override int Calculate(int firstNum, int secondNum)
+        {
+              int output = firstNum / secondNum;
+            return output;
+        }
+    }
+    public class Remainder : CalculateValue
+    {
+        public override int Calculate(int firstNum, int secondNum)
+        {
+              int output = firstNum / secondNum;
+            return output;
         }
     }
 }
